8675dc886419731071437c9145b2fc240c60bf8c
Owais Ahmed Hussain <owais.a.hussain@gmail.com>
2023-05-10 13:28:36 +0500

    Replaced FlatList with SectionList

--- App.js
+++ App.js
@@ -1,80 +1,95 @@
-import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View, SectionList } from 'react-native';
 import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
+import Login from './Login';
 
-const vehicleModels = ['Toyota Corolla', 'Honda Civic', 'Suzuki Mehran', 'Daihatsu Cuore', 'Honda CD-70', 'Toyota Hiace', 'Honda City', 'Suzuki Swift']
-
-const allVehicles = [
-  {'id': 1, 'name': "Honda CD70", 'capacity': 2 }, 
-  {'id': 2, 'name': "Honda CG125", 'capacity': 2 }, 
-  {'id': 3, 'name': "Honda City", 'capacity': 4 }, 
-  {'id': 4, 'name': "Honda Civic", 'capacity': 4 }, 
-  {'id': 5, 'name': "Honda Accord", 'capacity': 5 }, 
-  {'id': 6, 'name': "Honda BR-V", 'capacity': 4 }, 
-  {'id': 7, 'name': "Honda CR-Z", 'capacity': 4 }, 
-  {'id': 8, 'name': "Hyundai Elantra", 'capacity': 4 }, 
-  {'id': 9, 'name': "Hyundai Sonata", 'capacity': 4 }, 
-  {'id': 10, 'name': "Hyundai Tucson", 'capacity': 5 }, 
-  {'id': 11, 'name': "Hyundai Santro", 'capacity': 4 }, 
-  {'id': 12, 'name': "Hyundai Shehroz", 'capacity': 10 }, 
-  {'id': 13, 'name': "Kia Picanto", 'capacity': 4 }, 
-  {'id': 14, 'name': "Kia Sportage", 'capacity': 5 }, 
-  {'id': 15, 'name': "Kia Stonic", 'capacity': 5 }, 
-  {'id': 16, 'name': "Kia Grand Carnival", 'capacity': 7 }, 
-  {'id': 17, 'name': "MG ZS", 'capacity': 5 }, 
-  {'id': 18, 'name': "MG HS", 'capacity': 5 }, 
-  {'id': 19, 'name': "Suzuki Mehran", 'capacity': 4 }, 
-  {'id': 20, 'name': "Suzuki Alto", 'capacity': 4 }, 
-  {'id': 21, 'name': "Suzuki Bolan", 'capacity': 6 }, 
-  {'id': 22, 'name': "Suzuki Cultus", 'capacity': 4 }, 
-  {'id': 23, 'name': "Suzuki Wagon R", 'capacity': 4 }, 
-  {'id': 24, 'name': "Suzuki Swift", 'capacity': 4 }, 
-  {'id': 25, 'name': "Suzuki Baleno", 'capacity': 5 }, 
-  {'id': 26, 'name': "Suzuki FX", 'capacity': 4 }, 
-  {'id': 27, 'name': "Suzuki Liana", 'capacity': 4 }, 
-  {'id': 28, 'name': "Suzuki Khyber", 'capacity': 4 }, 
-  {'id': 29, 'name': "Suzuki Margalla", 'capacity': 4 }, 
-  {'id': 30, 'name': "Suzuki Potohar", 'capacity': 6 }, 
-  {'id': 31, 'name': "Suzuki GD110S", 'capacity': 2 }, 
-  {'id': 32, 'name': "Suzuki GSX125", 'capacity': 2 }, 
-  {'id': 33, 'name': "Suzuki GS150", 'capacity': 2 }, 
-  {'id': 34, 'name': "Suzuki Sprinter", 'capacity': 2 }, 
-  {'id': 35, 'name': "Toyota Corolla", 'capacity': 4 }, 
-  {'id': 36, 'name': "Toyota Camry", 'capacity': 4 }, 
-  {'id': 37, 'name': "Toyota Prius", 'capacity': 4 }, 
-  {'id': 38, 'name': "Toyota Vitz", 'capacity': 4 }, 
-  {'id': 39, 'name': "Toyota Yaris", 'capacity': 4 }, 
-  {'id': 40, 'name': "Toyota Hilux", 'capacity': 9 }, 
-  {'id': 41, 'name': "Toyota Fortuner", 'capacity': 6 }, 
-  {'id': 42, 'name': "Toyota Land Cruiser", 'capacity': 6 }, 
-  {'id': 44, 'name': "Toyota Hiace", 'capacity': 13 }, 
-  {'id': 45, 'name': "Toyota Coaster", 'capacity': 22 }, 
-  {'id': 46, 'name': "Yamaha YBR125", 'capacity': 2 }
+const allVehicles = [ 
+  {
+    "type": "Sedan",
+    "data": [
+      {'id': 1, 'name': "Honda City", 'capacity': 4 }, 
+      {'id': 2, 'name': "Honda Civic", 'capacity': 4 }, 
+      {'id': 3, 'name': "Honda Accord", 'capacity': 5 }, 
+      {'id': 4, 'name': "Toyota Corolla", 'capacity': 4 }, 
+      {'id': 5, 'name': "Toyota Camry", 'capacity': 4 }, 
+      {'id': 6, 'name': "Toyota Prius", 'capacity': 4 }, 
+    ],
+  },
+  {
+    "type": "Hatch back", 
+    "data": [
+      {'id': 7, 'name': "Honda Fit", 'capacity': 5 }, 
+      {'id': 8, 'name': "Suzuki Mehran", 'capacity': 4 }, 
+      {'id': 9, 'name': "Suzuki Alto", 'capacity': 4 }, 
+      {'id': 10, 'name': "Suzuki Cultus", 'capacity': 4 }, 
+      {'id': 11, 'name': "Suzuki Wagon R", 'capacity': 4 }, 
+      {'id': 12, 'name': "Suzuki Swift", 'capacity': 4 }, 
+      {'id': 13, 'name': "Toyota Vitz", 'capacity': 4 }, 
+      {'id': 14, 'name': "Toyota Passo", 'capacity': 4 }, 
+      {'id': 15, 'name': "Hyundai Santro", 'capacity': 4 }, 
+    ]
+  },
+  {
+    "type": "SUV",
+    "data": [
+      {'id': 16, 'name': "Hyundai Tucson", 'capacity': 5 }, 
+      {'id': 17, 'name': "Kia Sportage", 'capacity': 5 }, 
+      {'id': 18, 'name': "Toyota Hilux", 'capacity': 9 }, 
+      {'id': 19, 'name': "Toyota Land Cruiser", 'capacity': 6 }, 
+    ]
+  },
+  {
+    "type": "Motorcycle",
+    "data": [
+      {'id': 20, 'name': "Kawasaki GTO110", 'capacity': 2 }, 
+      {'id': 21, 'name': "Suzuki GSX125", 'capacity': 2 }, 
+      {'id': 22, 'name': "Suzuki GS150", 'capacity': 2 }, 
+      {'id': 23, 'name': "Yamaha YBR125", 'capacity': 2 },
+    ]
+  },
+  {
+    "type": "Van",
+    "data": [
+      {'id': 24, 'name': "Suzuki Bolan", 'capacity': 6 }, 
+      {'id': 25, 'name': "Toyota Hiace", 'capacity': 13 },     
+      {'id': 26, 'name': "Hyundai Shehroz", 'capacity': 10 }, 
+      {'id': 27, 'name': "Kia Grand Carnival", 'capacity': 7 }, 
+    ]
+  }
 ]
 
 export default function App() {
 
-  // Vehicle item component
-  const Item = ({name, capacity}) => {
-    return (
-      <View>
-        <Text style={styles.menuItem}>{name} ({capacity})</Text>
-      </View>
-    )
-  }
+// Render the headers of section. Note that the input prop is section and we're using 'type' attribute inside
+const renderSectionHeader = ({ section }) => {
+  return (<Text>{section.type}</Text>);
+};
 
-  // Handler function to render a single Vehicle item
-  const renderVehicleItem = ({item}) => <Item name={item.name} capacity={item.capacity}></Item>
+// Render each menu item
+const renderVehicleModelItem = ({ item }) => {
+  return (
+    <View>
+      <Text>{item.name} ({item.capacity})</Text>
+    </View>
+  );
+}  
+  
+// Separator separates items. We're only using an empty view with border for now
+const itemSeparatorComponent = () => <View style={{ borderColor: 'black', borderStyle: "dotted", borderWidth: 1 }}></View>;
 
   return (
-    // Demo: Flat List
+    // Demo: Section List
     <View style={styles.container}>
       {/* Attach header component */}
       <AppHeader />
+      <Login />
       <View style={styles.mainContainer}>
-        <FlatList 
-          data={allVehicles}
-          renderItem={renderVehicleItem}
+        <SectionList 
+        sections={allVehicles}
+        renderItem={renderVehicleModelItem}
+        renderSectionHeader={renderSectionHeader}
+        ItemSeparatorComponent={itemSeparatorComponent}
+        keyExtractor={(item, index) => item.id * (item.id + index)}
         />
       </View>
       {/* Attach footer component */}
