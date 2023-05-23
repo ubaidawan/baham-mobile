e0070bd3ec0175fa2c2edcdf9e6e4f773921aaf2
Esquared Systems <owais.a.hussain@gmail.com>
2023-05-23 15:27:20 +0500

    Tab navigation implemented

--- App.js
+++ App.js
@@ -5,18 +5,46 @@ import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
 import Login from './Login';
 import Menu from './Menu';
+import About from './About';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <View style={styles.container}>
         <AppHeader />
-          <Stack.Navigator initialRouteName='Login' screenOptions={{headerStyle: {backgroundColor: 'lightyellow'}}}>
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Menu" component={Menu} />
-          </Stack.Navigator>
+        {/* <Stack.Navigator initialRouteName='Login' screenOptions={{headerStyle: {backgroundColor: 'lightyellow'}}}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Menu" component={Menu} />
+        </Stack.Navigator> */}
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName = 'device-unknown';
+              if (route.name === 'About') {
+                iconName = focused
+                  ? 'info'
+                  : 'info-outline';
+              } else if (route.name === 'Menu') {
+                iconName = focused
+                  ? 'menu-open'
+                  : 'menu';
+              } else if (route.name === 'Logout') {
+                iconName = 'logout';
+              }
+              return <MaterialIcons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'navy',
+            tabBarInactiveTintColor: 'gray',
+          })}>
+          <Tab.Screen name="Menu" component={Menu} />
+          <Tab.Screen name="About" component={About} />
+        </Tab.Navigator>
         <AppFooter />
       </View>
     </NavigationContainer>
